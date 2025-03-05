7e4ae7906389b8fceed5850e8600e34e35b9973b
kennedh <ken.cmarcellino@gmail.com>
2025-03-05 19:21:25 -0300

    Update desafio nivel3

--- desafio.py
+++ desafio.py
@@ -47,8 +47,30 @@ class Carro:
         if isinstance(novo_ano, int) and novo_ano > 1886:
             self.__ano = novo_ano
 
+class CarroEsportivo(Carro):
+    def __init__(self, marca, modelo, ano, ligado=False, turbo=False):
+        super().__init__(marca, modelo, ano, ligado=False)
+        self.turbo = turbo
+
+    def exibir_info(self):
+        super().exibir_info()
+        print(f"Turbo: {"Sim" if self.turbo else "Não"}")
+
+class CarroEletrico(Carro):
+    def __init__(self, marca, modelo, ano, autonomia, ligado=False):
+        super().__init__(marca, modelo, ano, ligado=False)
+        self.autonomia = autonomia
+
+    def exibir_info(self):
+        super().exibir_info()
+        print(f"Autonomia: {self.autonomia} horas")
+
 c1 = Carro("Volkswagen", "Gol", 2002)
 c1.exibir_info()
 
-c1.modelo = "fox"
-print(c1.modelo)
+esportivo = CarroEsportivo("Ferrari", "F8", 2023,True, True)
+
+eletrico = CarroEletrico("Tesla", "Model S", 2022, 20)
+
+esportivo.exibir_info()
+eletrico.exibir_info()
